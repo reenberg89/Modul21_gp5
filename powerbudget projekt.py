79db12bb027c5d3da6d7af89916db915d52c1a76
reenberg89 <50106648+reenberg89@users.noreply.github.com>
2019-05-21 09:23:19 +0200

    Add files via upload

--- powerbudget projekt.py
+++ powerbudget projekt.py
@@ -1,5 +1,11 @@
+# importer math for senere at bruge funktionen ceil. Denne funktion runder værdien op til nærmeste hele tal.
+#global a er længden som gennem i denne variabel
 import math
 global a
+
+#############################################################################################################
+
+# menu giver brugeren valgmuligheden for at vælge single mode, multi mode eller at stoppe denne beregner.
 def menu():
     global valg
     global x
@@ -20,6 +26,8 @@ def menu():
     elif valg==0:
         return stop()
 
+#længden indtastes her, hvis der er valgt multimode og længden smatidig overskrider 2 km vil en tekst fremkomme samt brugeren returneres til menu.
+# Ellers returneres der til statet passiver.
 def længde():
     global a
     print('indtast længden på strækningen')
@@ -29,6 +37,8 @@ def længde():
         return menu()
     else: return Passiver()
 
+#passiver er et state hvor brugeren skal indtaste antal konnekteringer og splidsninger, ydermere bruges math funktionen her sammen med længden af fiberen,
+#til at fastlægge en reparations margin.
 def Passiver():
     global sikkerhed
     global splids
@@ -45,6 +55,8 @@ def Passiver():
     if valg==2:
         return mm_udregn()
 
+#udregner hvor stort overskud der skal være meller SFP sender og modtager på 850nm samt 1300nm,
+#der returneres til menu
 def mm_udregn():
     print('for at kunne klare strækningen skal sfp modulerne have et brutto overskud på:')
     B850=splids+kon+(a*x)+sikkerhed+rep
@@ -53,7 +65,8 @@ def mm_udregn():
     print('1300nm =','%4.2f'%B1300,'dB')
     return menu()
 
-
+#udregner hvor stort overskud der skal være meller SFP sender og modtager på 850nm samt 1300nm,
+#der returneres til menu
 def sm_udregn():
     print('for at kunne klare strækningen skal sfp modulerne have et brutto overskud på:')
     B1310=splids+kon+(a*x)+sikkerhed+rep
@@ -61,6 +74,8 @@ def sm_udregn():
     B1550=splids+kon+(a*y)+sikkerhed+rep
     print('1550nm =','%4.2f'%B1550,'dB')
     return menu()
+
+#stop statet printer en tekst og returnere til None som i dette program stopper beregneren.
 def stop():
     print('beregneren er stoppet')
     return None
